29421ce50322268f447bb23c7ff1f662430cd1b4
mathmartins2 <matheusgleiciel@gmail.com>
2023-06-13 17:50:16 -0300

    f

--- back/prisma/migrations/20230613194309_/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Condominium" ALTER COLUMN "deletedAt" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Order" ALTER COLUMN "deletedAt" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Resident" ALTER COLUMN "deletedAt" DROP NOT NULL;
