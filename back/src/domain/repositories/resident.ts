8a97be3410eab0efe4eb90eebe91a181dae26d43
vsantos1711 <vsantos067100@gmail.com>
2023-06-13 17:37:30 -0300

    feat/resident repository list method

--- back/src/domain/repositories/resident.ts
+++ back/src/domain/repositories/resident.ts
@@ -3,6 +3,7 @@ export interface ResidentRepositoryInterface {
   create(resident: Resident): Promise<Resident>;
   delete(id: string): Promise<void>;
   update(resident: Resident): Promise<Resident>;
+  list(): Promise<Resident[]>;
   findByPhoneNumber(phoneNumber: string): Promise<Resident>;
   findByName(name: string): Promise<Resident>;
   findByBuildingApartment(name: string): Promise<Resident>;
