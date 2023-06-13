8bd1a3593f4aefea61ef78471742cd17052fdaff
vsantos1711 <vsantos067100@gmail.com>
2023-06-13 15:03:58 -0300

    Resident repository interface

---
+++ back/src/domain/repositories/resident.ts
@@ -0,0 +1,9 @@
+import { Resident } from '../entities/resident';
+export interface ResidentRepositoryInterface {
+  create(resident: Resident): Promise<Resident>;
+  delete(id: string): Promise<void>;
+  update(resident: Resident): Promise<Resident>;
+  findByPhoneNumber(phoneNumber: string): Promise<Resident>;
+  findByName(name: string): Promise<Resident>;
+  findByBuildingApartment(name: string): Promise<Resident>;
+}
