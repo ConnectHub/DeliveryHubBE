199875a6d17a8141fdb5a630ea4af581a3a6da8c
vsantos1711 <vsantos067100@gmail.com>
2023-06-14 16:13:36 -0300

    feat: service find resident

--- back/src/application/resident/resident.service.ts
+++ back/src/application/resident/resident.service.ts
@@ -3,18 +3,22 @@ import { ResidentRepository } from './repository/resident.repository';
 import { Resident } from 'src/domain/entities/resident';
 import { NumberFormat } from './helpers/number-format';
 import { ResidentNotFound } from './errors/resident-not-found';
+import { ResidentAlreadyExist } from './errors/resident-already-exists';
 
 @Injectable()
 export class ResidentService {
   constructor(private residentRepository: ResidentRepository) {}
 
   async createResident(resident: Resident): Promise<Resident> {
+    await this.findResident(resident);
     resident.phoneNumber = NumberFormat.format(resident.phoneNumber);
     return await this.residentRepository.create(resident);
   }
-  
-  async findResident(resident: Resident) {
-    return await this.residentRepository.findResident(resident);
+
+  async findResident(resident: Resident): Promise<Resident> {
+    const prevResident = await this.residentRepository.findResident(resident);
+    if (prevResident) throw new ResidentAlreadyExist();
+    return prevResident;
   }
 
   async listAllResidents(): Promise<Resident[]> {
