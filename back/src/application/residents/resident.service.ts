892594d3543e73a3261936a5fb24c958cc220a08
vsantos1711 <vsantos067100@gmail.com>
2023-06-13 22:03:59 -0300

    feat:Number format on createResident

--- back/src/application/residents/resident.service.ts
+++ back/src/application/residents/resident.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ResidentRepository } from './repository/resident.repository';
 import { Resident } from 'src/domain/entities/resident';
+import { NumberFormat } from './helpers/number-format';
 
 @Injectable()
 export class ResidentService {
   constructor(private residentRepository: ResidentRepository) {}
 
   async createResident(resident: Resident): Promise<Resident> {
+    resident.phoneNumber = NumberFormat.format(resident.phoneNumber);
+
     return await this.residentRepository.create(resident);
   }
 }
