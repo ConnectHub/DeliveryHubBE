eb66b4e65b185fafd294a56970390bd70c5afc33
vsantos1711 <vsantos067100@gmail.com>
2023-06-14 19:34:08 -0300

    refactor: length validator on update resident dto

--- back/src/application/resident/dto/update-resident.dto.ts
+++ back/src/application/resident/dto/update-resident.dto.ts
@@ -10,23 +10,30 @@ export class UpdateResidentDto {
   @IsNotEmpty()
   @IsString()
   @Length(3, 40)
+  @IsOptional()
   name: string;
 
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   condominiumId: string;
 
   @IsNotEmpty()
   @IsString()
-  phoneNumber: string;
-
-  @IsString()
-  @IsEmail()
   @IsOptional()
-  email?: string;
+  @Length(3, 20)
+  phoneNumber: string;
 
   @IsNotEmpty()
   @IsString()
+  @IsEmail()
+  @IsOptional()
+  @Length(3, 30)
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
   @Length(3, 15)
   buildingApartment: string;
 }
