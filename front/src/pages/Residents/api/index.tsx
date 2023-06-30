9b650079d034d63e5757f039e35c93c7cabda5f1
vsantos1711 <vsantos067100@gmail.com>
2023-06-29 23:01:13 -0300

    feat: new properties on Resident interface

--- front/src/pages/Residents/api/index.tsx
+++ front/src/pages/Residents/api/index.tsx
@@ -2,6 +2,9 @@ import { api } from '../../../services/api';
 
 interface Resident {
   name: string;
+  buildingApartment: string;
+  phoneNumber: string;
+  email: string;
 }
 export async function getResidents(): Promise<Resident[]> {
   return (await api('/resident/list')).data;
