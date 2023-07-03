2eebbdd781cfd7511ec9aa15a09e7814fc25b849
vsantos1711 <vsantos067100@gmail.com>
2023-07-03 15:03:49 -0300

    feat: Loading Component

---
+++ front/src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { Spin } from "antd";
+
+export function LoadingComponent() {
+  return(
+    <div className="my-20">
+        <Spin tip="Carregando..." size="large">
+          <div className="content" />
+        </Spin>
+    </div>
+  );
+}
