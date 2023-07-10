96375155b2a9a5879bfcbddfe187c80a00cec066
Matheus Martins <78503039+mathmartins2@users.noreply.github.com>
2023-07-10 13:01:51 -0300

    Update index.tsx

--- front/src/pages/Dashboard/index.tsx
+++ front/src/pages/Dashboard/index.tsx
@@ -5,12 +5,11 @@ function DashboardPage() {
   return(  
     <>
       <h1 className="text-6xl text-center">Dashboard</h1>
-  
+      <h2 className="text-6xl text-center">work in progress</h2>
       <div className="grid grid-cols-2">
         {<PieChart title="Total de entregas por status" queryFunction={getTotalByStatus} />}
         {<PieChart title="Aqui tá só de exemplo" queryFunction={getTotalByStatus} />}
       </div>
-     
     </>
   );
 }
