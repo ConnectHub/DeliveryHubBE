c1c7e9fd87b03f5918971c3d6f4463b9dd8aee1a
Matheus Martins <78503039+mathmartins2@users.noreply.github.com>
2023-07-10 12:56:51 -0300

    Update index.tsx

--- front/src/components/Error/index.tsx
+++ front/src/components/Error/index.tsx
@@ -6,7 +6,7 @@ function GlitchError({ text = "ERROR!" }: GlitchErrorProps) {
   return (
     <div className="flex justify-center">
       <h2
-        className="text-center text-black text-4xl font-bold "
+        className="text-center text-black text-4xl font-bold"
         style={{ textShadow: "red -2px 0, cyan 2px 0" }}
       >
         {text}
