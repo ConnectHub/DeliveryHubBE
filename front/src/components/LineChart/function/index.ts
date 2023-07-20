326a8f6c32fb446efd5b9a75e2b104015c9dd018
vsanto1711 <vsantos067100@gmail.com>
2023-07-20 16:04:35 -0300

    chore: start create config on Line Chart functions

---
+++ front/src/components/LineChart/function/index.ts
@@ -0,0 +1,38 @@
+import { ChartData } from "../interfaces";
+
+export function configCreate(data: ChartData[]) {
+  return(
+    {
+      data,
+      xField: 'month',
+      yField: 'value',
+      label: {},
+      point: {
+        size: 5,
+        shape: 'diamond',
+        style: {
+          fill: 'white',
+          stroke: '#5B8FF9',
+          lineWidth: 2,
+        },
+      },
+      tooltip: {
+        showMarkers: false,
+      },
+      state: {
+        active: {
+          style: {
+            shadowBlur: 4,
+            stroke: '#000',
+            fill: 'red',
+          },
+        },
+      },
+      interactions: [
+        {
+          type: 'marker-active',
+        },
+      ],
+    }
+  )
+}
