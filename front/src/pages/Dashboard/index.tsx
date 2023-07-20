3efe98e4ec76b9759586e42bf763914fc17e943e
vsanto1711 <vsantos067100@gmail.com>
2023-07-20 20:02:30 -0300

    ...

--- front/src/pages/Dashboard/index.tsx
+++ front/src/pages/Dashboard/index.tsx
@@ -6,10 +6,11 @@ function DashboardPage() {
   return (
     <>
       <h1 className="text-6xl text-center">Dashboard</h1>
+      <h1 className="text-6xl text-center">work in progress...</h1>
 
       <div className="flex justify-center items-center flex-col gap-5">
         <div>
-          <LineChart title="Total de entregas por mês" />
+          <LineChart title="Total de entregas por mês EXEMPLO" />
         </div>
         <div className="flex gap-4 w-full justify-around">
           <PieChart
@@ -17,7 +18,7 @@ function DashboardPage() {
             queryFunction={getTotalByStatus}
           />
           <PieChart
-            title="Aqui tá só de exemplo"
+            title="Aqui tá só de teste EXEMPLO"
             queryFunction={getTotalByStatus}
           />
         </div>
