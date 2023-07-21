3d0f734cfd5c5cf2f2e45d8ee0c11c4781b105d9
mathmartins2 <matheusgleiciel@gmail.com>
2023-07-21 19:26:27 -0300

    feat: add rate

--- front/src/components/LineChart/function/index.ts
+++ front/src/components/LineChart/function/index.ts
@@ -1,38 +1,36 @@
-import { ChartData } from "../interfaces";
+import { ChartData } from '../interfaces';
 
 export function configCreate(data: ChartData[]) {
-  return(
-    {
-      data,
-      xField: 'month',
-      yField: 'value',
-      label: {},
-      point: {
-        size: 5,
-        shape: 'diamond',
+  return {
+    data,
+    xField: 'month',
+    yField: 'value',
+    label: {},
+    point: {
+      size: 5,
+      shape: 'diamond',
+      style: {
+        fill: 'white',
+        stroke: '#5B8FF9',
+        lineWidth: 2,
+      },
+    },
+    tooltip: {
+      showMarkers: false,
+    },
+    state: {
+      active: {
         style: {
-          fill: 'white',
-          stroke: '#5B8FF9',
-          lineWidth: 2,
+          shadowBlur: 4,
+          stroke: '#000',
+          fill: 'red',
         },
       },
-      tooltip: {
-        showMarkers: false,
+    },
+    interactions: [
+      {
+        type: 'marker-active',
       },
-      state: {
-        active: {
-          style: {
-            shadowBlur: 4,
-            stroke: '#000',
-            fill: 'red',
-          },
-        },
-      },
-      interactions: [
-        {
-          type: 'marker-active',
-        },
-      ],
-    }
-  )
+    ],
+  };
 }
