a64cb46662858d5891ef9ac5409ef5800bd1da42
vsanto1711 <vsantos067100@gmail.com>
2023-07-30 19:52:45 -0300

    chore: finalize Pie chart

--- front/src/components/PieChart/index.tsx
+++ front/src/components/PieChart/index.tsx
@@ -1,50 +1,34 @@
 import { Pie } from '@ant-design/charts';
-import { useQuery } from 'react-query';
 import LoadingComponent from '../Loading';
 import GlitchError from '../Error';
-import { ChartData } from './interfaces';
 import { createConfig } from './function';
+import { UseQueryResult } from 'react-query';
+import { ChartData } from '../../pages/Dashboard/interfaces';
 
 interface PieChartData {
-  // queryFunction: () => Promise<ChartData[]>;
+  queryFunction: () => UseQueryResult<ChartData[], Error>;
   title: string;
 }
 
-function PieChart({ title }: PieChartData) {
-  // const { isLoading, error, data } = useQuery<ChartData[], Error>(
-  //   'queryData',
-  //   queryFunction,
-  // );
+function PieChart({ title, queryFunction }: PieChartData) {
+  const { isLoading, error, data } = queryFunction();
 
-  const data = [
-    {
-      description: 'PENDENTE',
-      total: 10,
-    },
-    {
-      description: 'ENTREGUE',
-      total: 10,
-    },
-    {
-      description: 'CANCELADAS',
-      total: 10,
-    },
-  ];
-
-  const isLoading = false;
-  const error = false;
   const config = createConfig(data ?? []);
 
   return (
-    <div className=" bg-primary p-2 rounded sm:min-w-[500px] hover:scale-[1.01]">
-      <div className="p-2 rounded bg-slate-50 ">
-        <h2 className="mb-2 text-3xl font-semibold text-left">{title}</h2>
-        {isLoading && <LoadingComponent />}
-        {error && <GlitchError text="ERROR AO GERAR O GRÁFICO" />}
-        {data && data?.length == 0 && (
-          <GlitchError text="Não há dados para mostrar!" />
-        )}
-        {data && data?.length > 0 && <Pie {...config} />}
+    <div className=" bg-primary p-2 rounded sm:min-w-[500px] hover:scale-[1.01] min-h-full ">
+      <div className="flex flex-col items-center min-h-full p-2 rounded justify-items-start bg-slate-50">
+        <h2 className="w-full mb-2 text-3xl font-semibold text-left">
+          {title}
+        </h2>
+        <div className="flex flex-col items-center justify-center flex-grow">
+          {isLoading && <LoadingComponent />}
+          {error && <GlitchError text="ERROR AO GERAR O GRÁFICO" />}
+          {data && data?.length == 0 && (
+            <GlitchError text="Ainda não há dados para mostrar!" />
+          )}
+          {data && data?.length > 0 && <Pie {...config} />}
+        </div>
       </div>
     </div>
   );
