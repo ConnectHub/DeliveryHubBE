a1f91b1544960e16455a1faac1ef781e4924b547
vsanto1711 <vsantos067100@gmail.com>
2023-07-30 19:54:05 -0300

    chore: integrate dashboard module

--- front/src/pages/Dashboard/index.tsx
+++ front/src/pages/Dashboard/index.tsx
@@ -2,41 +2,59 @@ import BarChart from '../../components/BarChart';
 import InformationCard from '../../components/InformationCard';
 import LineChart from '../../components/LineChart';
 import PieChart from '../../components/PieChart';
+import {
+  useGetOrderByStatus,
+  useGetTotalOrdersDelivered,
+  useGetTotalResidents,
+  useGetTotalOrdersPending,
+  useGetOrdersByMonth,
+  useGetOrdersByCondominium,
+} from './api/service';
 
 function DashboardPage() {
+  const { data: ordersDelivered } = useGetTotalOrdersDelivered();
+  const { data: countResidents } = useGetTotalResidents();
+  const { data: ordersPending } = useGetTotalOrdersPending();
+
   return (
     <>
       <h1 className="pb-4 text-4xl text-left">Dashboard</h1>
-      <p className="text-lg font-bold text-left">
-        work in progress... (miss the back)
-      </p>
 
       <div className="flex flex-col gap-5 ">
         <div className="flex flex-col p-2 sm:flex-row gap-7">
           <InformationCard
             description="Total de encomendas entregues"
-            total={1400}
-            backgroundColor="bg-violet-700"
+            total={ordersDelivered ?? 0}
+            backgroundColor="bg-green-500"
           />
           <InformationCard
             description="Total de moradores cadastrados"
-            total={15}
-            backgroundColor="bg-blue-700"
+            total={countResidents ?? 0}
+            backgroundColor="bg-blue-600"
           />
           <InformationCard
             description="Total de encomendas pendentes"
-            total={7}
-            backgroundColor="bg-orange-700"
+            total={ordersPending ?? 0}
+            backgroundColor="bg-zinc-700"
           />
         </div>
 
         <div className="flex flex-col gap-10 p-2 xl:flex-row">
-          <PieChart title="Entregas por status" />
-          <LineChart title="Entregas por mês" />
+          <PieChart
+            title="Entregas por status"
+            queryFunction={useGetOrderByStatus}
+          />
+          <LineChart
+            title="Entregas por mês"
+            queryFunction={useGetOrdersByMonth}
+          />
         </div>
 
         <div className="p-2">
-          <BarChart title="Entregas por condomínio" />
+          <BarChart
+            title="Entregas por condomínio"
+            queryFunction={useGetOrdersByCondominium}
+          />
         </div>
       </div>
     </>
