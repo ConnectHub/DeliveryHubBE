d867546e4cfc53113716be672db0d64a5c519bb6
vsanto1711 <vsantos067100@gmail.com>
2023-07-30 19:49:54 -0300

    chore: finalize Bar chart

--- front/src/components/BarChart/index.tsx
+++ front/src/components/BarChart/index.tsx
@@ -1,39 +1,28 @@
 import { Bar } from '@ant-design/charts';
-import { ChartData } from './interfaces';
+import { ChartData } from '../../pages/Dashboard/interfaces';
+import { UseQueryResult } from 'react-query';
+import { createConfig } from '../BarChart/function';
+import LoadingComponent from '../Loading';
+import GlitchError from '../Error';
 
 interface BarChartData {
-  queryFunction?: () => Promise<ChartData[]>;
+  queryFunction: () => UseQueryResult<ChartData[], Error>;
   title: string;
 }
-function BarChart({ title }: BarChartData) {
-  const data = [
-    {
-      year: 'VINICIN',
-      value: 145,
-    },
-    {
-      year: 'MATHIAS MATHEUS',
-      value: 61,
-    },
-    {
-      year: 'GIEDO TUDO NOS CONFORMES',
-      value: 52,
-    },
-    {
-      year: 'DRACULA',
-      value: 15,
-    },
-  ];
-  const config = {
-    data,
-    xField: 'value',
-    yField: 'year',
-    seriesField: 'year',
-  };
+function BarChart({ title, queryFunction }: BarChartData) {
+  const { isLoading, error, data } = queryFunction();
+
+  const config = createConfig(data ?? []);
+
   return (
     <div className="bg-primary p-2 rounded sm:min-w-[500px] hover:scale-[1.01]">
       <div className="p-2 rounded bg-slate-50 ">
         <h2 className="mb-2 text-3xl font-semibold text-left">{title}</h2>
+        {isLoading && <LoadingComponent />}
+        {error && <GlitchError text="ERROR AO GERAR O GRÁFICO" />}
+        {data && data?.length == 0 && (
+          <GlitchError text="Ainda não há dados para mostrar!" />
+        )}
         {data && data?.length > 0 && <Bar {...config} />}
       </div>
     </div>
