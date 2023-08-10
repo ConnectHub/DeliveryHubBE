1207ab7b831357da19a368bcdfa2ba4ff27acba5
vsanto1711 <vsantos067100@gmail.com>
2023-08-09 21:15:41 -0300

    feat: add params on repository methods

--- back/src/domain/repositories/dashboard.ts
+++ back/src/domain/repositories/dashboard.ts
@@ -1,9 +1,10 @@
 import { ChartDataInterface } from 'src/application/dashboard/interfaces';
 
 export interface DashboardRepositoryInterface {
-  allDeliveredOrders(): Promise<number>;
-  totalResidents(): Promise<number>;
-  totalOrdersPending(): Promise<number>;
-  listOrdersByStatus(): Promise<ChartDataInterface[]>;
-  totalOrdersByMonths(): Promise<ChartDataInterface[]>;
+  allDeliveredOrders(id: string): Promise<number>;
+  totalResidents(id: string): Promise<number>;
+  totalOrdersPending(id: string): Promise<number>;
+  listOrdersByStatus(id: string): Promise<ChartDataInterface[]>;
+  totalOrdersByMonths(id: string): Promise<ChartDataInterface[]>;
+  listOrdersByCondominium(): Promise<ChartDataInterface[]>;
 }
