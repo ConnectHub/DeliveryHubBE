be995e083499f53f19a2a4e9b2e6ba9efaeca75f
vsantos1711 <vsantos067100@gmail.com>
2023-08-11 18:35:47 -0300

    fix: remove unnecessary method

--- back/src/application/dashboard/view-model/dashboard-view-model.ts
+++ back/src/application/dashboard/view-model/dashboard-view-model.ts
@@ -1,6 +1,5 @@
 import { translateStatus } from 'src/application/order/translator/order.translator';
 import { ChartDataInterface } from '../interfaces';
-import { FormatMonth } from '../../../infra/utils/format-month';
 
 export class DashboardViewModel {
   static toHttp(data: ChartDataInterface) {
@@ -10,47 +9,5 @@ export class DashboardViewModel {
       orderCount: data.orderCount,
       value: data.value,
     };
-  }
-
-  static aggregateOrdersByMonth(orders: ChartDataInterface[]) {
-    const monthNames = FormatMonth.monthNames;
-    const ordersByMonthMap: Record<string, number> = {};
-    monthNames.forEach((month) => {
-      ordersByMonthMap[month] = 0;
-    });
-
-    orders.forEach((order) => {
-      const orderDate = new Date(order.receiptDateHour);
-      const orderMonth = monthNames[orderDate.getMonth()];
-      ordersByMonthMap[orderMonth] += 1;
-    });
-
-    const ordersGroupedByMonth: ChartDataInterface[] = [];
-
-    Object.entries(ordersByMonthMap).forEach(([month, count]) => {
-      ordersGroupedByMonth.push({ month, orderCount: count });
-    });
-
-    return ordersGroupedByMonth;
-  }
-
-  static aggregateOrdersByCondominium(orders) {
-    const deliveriesByCondominium = orders.reduce((result, order) => {
-      const condominiumId = order.addressee?.condominiumId;
-      const condominiumName = order.addressee?.condominium?.name;
-      if (condominiumId && condominiumName) {
-        const foundCondominium = result.find(
-          (item) => item.condominiumName === condominiumName,
-        );
-        if (foundCondominium) {
-          foundCondominium.value += 1;
-        } else {
-          result.push({ condominiumName, value: 1 });
-        }
-      }
-      return result;
-    }, [] as { condominiumName: string; value: number }[]);
-
-    return deliveriesByCondominium;
   }
 }
