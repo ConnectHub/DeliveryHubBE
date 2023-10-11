5ab798760baea4123ba60015ca68b9360fc167be
Vinicius Francisco <104466068+vsantos1711@users.noreply.github.com>
2023-10-11 19:07:15 +0000

    test: add tests on list method in Resident Controller

--- src/application/resident/tests/resident.controller.spec.ts
+++ src/application/resident/tests/resident.controller.spec.ts
@@ -4,12 +4,15 @@ import { ResidentService } from '../resident.service';
 import { ResidentRepository } from '../repository/resident.repository';
 import { PrismaService } from 'src/infra/prisma/prisma.service';
 import { CreateResidentDto } from '../dto/create-resident.dto';
+import { Resident } from 'src/domain/entities/resident';
+import { RequestInterface } from 'src/application/auth/interfaces';
+import { ResidentViewModel } from '../view-model/resident-view-model';
 
 describe('ResidentController', () => {
   let residentController: ResidentController;
   let residentService: ResidentService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ResidentController],
       providers: [ResidentService, ResidentRepository, PrismaService],
@@ -22,10 +25,10 @@ describe('ResidentController', () => {
   describe('createResident', () => {
     it('should return a resident', async () => {
       const mockResident: CreateResidentDto = {
-        name: 'Vini santos',
+        name: 'Resident test',
         condominiumId: '12345678910',
         phoneNumber: '12345678910',
-        email: 'vini@test.com',
+        email: 'resident@test.com',
         buildingApartment: '123',
       };
 
@@ -39,5 +42,47 @@ describe('ResidentController', () => {
       expect(residentService.createResident).toHaveBeenCalledWith(mockResident);
       expect(residentService.createResident).toHaveBeenCalledTimes(1);
     });
+
+    it('should return a list of residents', async () => {
+      const condId = '12345';
+      const mockListOfResidents = [
+        {
+          name: 'Resident test 1',
+          condominiumId: condId,
+          phoneNumber: '(11) 12345-6789',
+          buildingApartment: '111',
+        },
+        {
+          name: 'Resident test 2',
+          condominiumId: condId,
+          phoneNumber: '(22) 12345-6789',
+          buildingApartment: '222',
+        },
+      ] as Resident[];
+      const mockReq = {
+        user: {
+          login: 'test',
+          sub: '12345678910',
+          condominiumId: condId,
+        },
+      } as RequestInterface;
+
+      jest
+        .spyOn(residentService, 'listAllResidents')
+        .mockResolvedValue(mockListOfResidents);
+
+      const result = await residentController.list(mockReq);
+
+      const formattedResult = mockListOfResidents.map((resident) =>
+        ResidentViewModel.toHttp(resident),
+      );
+
+      expect(result).toEqual(formattedResult);
+      expect(residentService.listAllResidents).toHaveBeenCalledWith(
+        mockReq.user.condominiumId,
+      );
+      expect(residentService.listAllResidents).toHaveBeenCalledTimes(1);
+    });
+    
   });
 });
