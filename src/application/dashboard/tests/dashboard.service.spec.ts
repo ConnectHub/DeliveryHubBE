971cad578353a8d5c251857c1eec2959eeff9b07
vsantos1711 <vsantos067100@gmail.com>
2023-10-22 12:57:29 -0300

    test: create DashboardService test

---
+++ src/application/dashboard/tests/dashboard.service.spec.ts
@@ -0,0 +1,21 @@
+import { Test } from '@nestjs/testing';
+import { PrismaModule } from 'src/infra/prisma/prisma.module';
+import { DashboardController } from '../dashboard.controller';
+import { DashboardService } from '../dashboard.service';
+import { DashboardRepository } from '../repository/dashboard.repository';
+
+describe('DashboardService', () => {
+  let dashboardService: DashboardService;
+  let dashboardRepository: DashboardRepository;
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [PrismaModule],
+      controllers: [DashboardController],
+      providers: [DashboardService, DashboardRepository],
+      exports: [DashboardService],
+    }).compile();
+
+    dashboardService = module.get<DashboardService>(DashboardService);
+    dashboardRepository = module.get<DashboardRepository>(DashboardRepository);
+  });
+});
