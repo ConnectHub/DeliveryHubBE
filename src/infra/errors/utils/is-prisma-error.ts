e76cb8bb3e890b33282d7a3ce418340c67efdeff
Vinicius Santos <104466068+vsantos1711@users.noreply.github.com>
2023-11-21 11:30:26 +0000

    Remove unused error handling code

--- src/infra/errors/utils/is-prisma-error.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClientError } from '../types/prisma-client.error';
-
-export const isPrismaError = (err: PrismaClientError) => {
-  return (
-    typeof err.code === 'string' &&
-    typeof err.clientVersion === 'string' &&
-    (typeof err.meta === 'undefined' || typeof err.meta === 'object')
-  );
-};
